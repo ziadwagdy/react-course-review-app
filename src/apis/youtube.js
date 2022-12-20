60061d2386042c7cdbd88fde7a8102c5068d61cf
ziadwagdy <zwagdy@gmail.com>
2022-12-20 19:39:56 +0300

    Update project

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = "AIzaSyAt3mMvC9rkSh4ueb6giNjLRIBH3JdELIM";
+
+export default axios.create({
+  baseURL: "https://youtube.googleapis.com/youtube/v3",
+  params : {
+    part : 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+
+  state = {term: ''};
+
+  onInputChange = e => {
+    this.setState({term: e.target.value})
+  }
+
+  onFormSubmit = e => {
+    e.preventDefault();
+
+  }
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <label>Video Search</label>
+          <input type="text" value={this.state.term}
+                 onChange={this.onInputChange}/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar; 
