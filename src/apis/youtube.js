29683d820492da83366e74976d09cc1c4f1db0ae
ziadwagdy <zwagdy@gmail.com>
2022-12-27 00:21:05 +0300

    last commit

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 const KEY = "AIzaSyAt3mMvC9rkSh4ueb6giNjLRIBH3JdELIM";
 
 export default axios.create({
-  baseURL: "https://youtube.googleapis.com/youtube/v3",
-  params : {
-    part : 'snippet',
-    maxResults: 5,
-    key: KEY
-  }
+    baseURL: "https://youtube.googleapis.com/youtube/v3",
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
 });
--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,51 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 
 class App extends React.Component {
 
-    state = {videos: []}
+    state = {videos: [], selectedVideo: null};
+
+    componentDidMount() {
+        this.onTermSubmit('learn');
+    }
+
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
-            q: term
-        })
+            params: {
+                q: term,
+            },
+        });
 
         this.setState({
-            videos: response.data.items
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
         });
     };
 
-    render() {
+    // passing props from a child to this parent
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video})
+    }
 
+    render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <div>I have {this.state.videos.length} videos</div>
-                <VideoList videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+
+                        <div className="five wide column ">
+                            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         );
     }
